657d25b9367d3356ff843750f7197f46d6731d12
Nurhadi Maulana <nurhadimaulana92@gmail.com>
2013-10-31 16:00:07 +0700

    Deleted

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :database_authenticatable, :confirmable, :invitable
-end
