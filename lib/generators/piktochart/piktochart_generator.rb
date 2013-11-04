24175374fd42317dd389a2190429b32074c0a670
Nurhadi Maulana <nurhadimaulana92@gmail.com>
2013-11-04 15:26:56 +0700

    Update

--- lib/generators/piktochart/piktochart_generator.rb
+++
@@ -1,7 +0,0 @@
-class PiktochartGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
-
-  def copy_initializer_file
-    copy_file "piktochart.rb", "config/initializers/piktochart.rb"
-  end
-end
